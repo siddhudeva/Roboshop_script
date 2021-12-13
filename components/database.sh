c9d814436ee7efc6b93288656c380dda459dee28
SiddhuDeva <siddhudangeti@gmail.com>
2021-12-13 15:46:07 +0530

    added roboshop script database- Mongodb added component added 1.1

--- components/database.sh
+++ components/database.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+#Author : siddhu
+#Date : 13/12/2021
+#Description : This is a Databases setup file for roboshop project
+source components/common.sh
+echo -e "\e[5;36m -----------------> Mongodb <--------------------\e[0m"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
+Status $? "Download mongodb"
 
-sample
+yum install -y mongodb-org &>>${LOG}
+Status $? "Install of mongodb"
+
+systemctl enable mongod &>>${LOG} && systemctl start mongod
+Status $? "Enabling and Starting of Mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+Status $? "Configuration"
+
+systemctl restart mongod &>>${LOG}
+Status $? "Restarting Mongodb"
+
+DOWNLOAD mongodb &>>${LOG}
+
+cd /tmp && unzip mongodb.zip &>>${LOG}
+Status $? "Extracting data"
+
+cd mongodb-main && mongo < catalogue.js &>>${LOG} && mongo < users.js &>>${LOG}
+Status $? "Schema added"
+
+echo -e "\e[5;36m -----------------> redis <--------------------\e[0m"
