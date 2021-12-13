d0be9a7d2597488b1c7b771592b009b42cf3288c
SiddhuDeva <siddhudangeti@gmail.com>
2021-12-13 16:44:38 +0530

    added roboshop script database- Mq sql added component added 1.0

--- components/database.sh
+++ components/database.sh
@@ -4,7 +4,7 @@
 #Description : This is a Databases setup file for roboshop project
 source components/common.sh
 
-echo -e "\e[6;36m -----------------> Mongodb <--------------------\e[0m"
+echo -e "\e[1;36m -----------------> Mongodb <--------------------\e[0m"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
 Status $? "Download mongodb"
 
@@ -28,7 +28,7 @@ Status $? "Extracting data"
 cd mongodb-main && mongo < catalogue.js &>>${LOG} && mongo < users.js &>>${LOG}
 Status $? "Schema added"
 
-echo -e "\e[6;36m -----------------> redis <--------------------\e[0m"
+echo -e "\e[1;36m -----------------> redis <--------------------\e[0m"
 
 curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG}
 Status $? "Repository setup"
@@ -43,7 +43,7 @@ Status $? "configaraitons are"
 systemctl enable redis &>>${LOG} && systemctl start redis
 Status $? "Start Redis Database"
 
-echo -e "\e[6;36m -----------------> rabbit mq <--------------------\e[0m"
+echo -e "\e[1;36m -----------------> rabbit mq <--------------------\e[0m"
 
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
 Status $? "Repository adding"
@@ -64,3 +64,43 @@ Status $? "Rabbitmq user added"
 
 rabbitmqctl set_user_tags roboshop administrator &>>${LOG} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
 Status $? "Rabbitmq user permissions"
+
+echo -e "\e[1;36m -----------------> MySQL <--------------------\e[0m"
+
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+Status $? "repositorie downloading"
+
+yum install mysql-community-server -y &>>${LOG}
+Status $? "MySQL installation"
+
+systemctl enable mysqld &>>${LOG} && systemctl start mysqld
+Status $? "MySQL enabling and starting"
+
+#Now a default root password will be generated and given in the log file.
+# grep temp /var/log/mysqld.log
+
+#Next, We need to change the default root password in order to start using the database service.
+# mysql_secure_installation
+
+#You can check the new password working or not using the following command.
+
+# mysql -u root -p
+
+#Run the following SQL commands to remove the password policy.
+#> uninstall plugin validate_password;
+#Setup Needed for Application.
+#As per the architecture diagram, MySQL is needed by
+
+#Shipping Service
+#So we need to load that schema into the database, So those applications will detect them and run accordingly.
+
+#To download schema, Use the following command
+
+# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+#Load the schema for Services.
+
+# cd /tmp
+# unzip mysql.zip
+# cd mysql-main
+# mysql -u root -pRoboShop@1 <shipping.sql
+
