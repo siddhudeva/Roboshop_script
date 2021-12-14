c06dc42cd3b5340d99cf10faa9205f86baa622b2
SiddhuDeva <siddhudangeti@gmail.com>
2021-12-14 16:05:23 +0530

    cart file added v1.0

--- components/common.sh
+++ components/common.sh
@@ -28,6 +28,7 @@ component=$1
    unzip /tmp/${component}.zip &>>${LOG}
    if [ ! -z "${Component}" ]; then
    rm -rf /home/roboshop/${component} &>>${LOG} && mkdir -p /home/roboshop/${component} && mv /tmp/${component}-main/* /home/roboshop/${component}
+ fi
  }
 
 
