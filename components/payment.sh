e715dea266ef5631633a455d9c2b3986bfeb224b
SiddhuDeva <siddhudangeti@gmail.com>
2021-12-14 23:47:20 +0530

    payment file updated V1.0

--- components/payment.sh
+++ components/payment.sh
@@ -2,36 +2,12 @@
 ##Author : siddhu
 ##Date : 13/12/2021
 ##Description : This is a Databases setup file for roboshop project
-
-
-
-
-
-
-
-
-
-
-#yum install python36 gcc python3-devel -y
-#Create a user for running the application
-## useradd roboshop
-#Download the repo.
-#$ cd /home/roboshop
-#$ curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
-#$ unzip /tmp/payment.zip
-#$ mv payment-main payment
-#$ cd payment
-#Install the dependencies
-## cd /home/roboshop/payment
-## pip3 install -r requirements.txt
-#Note: Above command may fail with permission denied, So run as root user
-#
-#Update the roboshop user and group id in payment.ini file.
-#
-#Setup the service
-#
-## mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
-## systemctl daemon-reload
-## systemctl enable payment
-## systemctl start payment
-
+source components/common.sh
+yum install python36 gcc python3-devel -y &>>${LOG}
+Status $? "PYTHON36 installation"
+USER
+DOWNLOAD payment
+pip3 install -r requirements.txt &>>${LOG}
+Status $? "Downloading Dependncies"
+CONFIG payment
+DAEMON payment
