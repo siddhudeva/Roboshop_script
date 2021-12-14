72efd0410b8aac5b57074107938d7af994a5a155
SiddhuDeva <siddhudangeti@gmail.com>
2021-12-14 23:35:47 +0530

    shipping file added v 1.0

--- components/common.sh
+++ components/common.sh
@@ -46,7 +46,7 @@ sed -i -e 's/MONGO_DNSNAME/mongod.roboshop.internal/' \
  -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
  -e 's/MONGO_ENDPOINT/mongod.roboshop.internal/' \
  -e 's/DBHOST/mysql.roboshop.internal/' \
- -e 's/CARTENDPOINT/mysql.roboshop.internal/' \
+ -e 's/CARTENDPOINT/cart.roboshop.internal/' \
  -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${component}/systemd.service &>>${LOG}
 Status $? "config file updation"
 
