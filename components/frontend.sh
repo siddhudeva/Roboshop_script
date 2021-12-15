c262fdaa2f7aba3fa39a61539180a4d41cc80145
SiddhuDeva <siddhudangeti@gmail.com>
2021-12-15 15:35:54 +0530

    original frontend file

--- components/frontend.sh
+++ components/frontend.sh
@@ -22,10 +22,10 @@ rm -rf frontend-master static README.md &>>${LOG}
 Status $? "removing unwanted files"
 
 sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/'/usr/share/nginx/html/localhost.conf &>>${LOG}
-sed -i -e '/cart/ s/localhost/cart.roboshop.internal/' /usr/share/nginx/html/localhost.conf &>>${LOG}
-sed -i -e '/payment/ s/localhost/payment.roboshop.internal/' /usr/share/nginx/html/localhost.conf &>>${LOG}
-sed -i -e '/shipping/ s/localhost/shipping.roboshop.internal/' /usr/share/nginx/html/localhost.conf &>>${LOG}
 sed -i -e '/user/ s/localhost/user.roboshop.internal/' /usr/share/nginx/html/localhost.conf &>>${LOG}
+sed -i -e '/cart/ s/localhost/cart.roboshop.internal/' /usr/share/nginx/html/localhost.conf &>>${LOG}
+sed -i -e '/shipping/ s/localhost/shipping.roboshop.internal/' /usr/share/nginx/html/localhost.conf &>>${LOG}
+sed -i -e '/payment/ s/localhost/payment.roboshop.internal/' /usr/share/nginx/html/localhost.conf &>>${LOG}
 Status $? "Configration file location changed"
 
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
