b2a850b7141838fd44772bb630077cb53daba2f5
siddhu <siddhudangeti@gmail.com>
2021-12-18 16:21:06 +0530

    ec2 instance filtering and creating

---
+++ ansible_robo/Robo Ec2/EC2setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: siddhu
+# Date : 18-12-2021
+# this file is to verify the Roboshop - setup ec2 instance
+
+EC2 ${component
+}
+
+
---
+++ ansible_robo/Robo Ec2/common.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Author: siddhu
+# Date : 18-12-2021
+# this file is to verify the common file for setup ec2 instance
+
+USER=$(id -u)
+if [ "$USER" -ne 0 ]; then
+   echo -e "\e[1;31m You are not a authorised person to run this\e[0m"
+   exit
+fi
+
+#validating tag mantioned or not
+   component=${1}
+if [ -z ${component} ]; then
+   echo -e "\e[1;31mPlease enter a tag name so that we can add a instance\e[0m"
+ exit
+fi
+
+EC() {
+component=$1
+
+aws ec2 describe-instances --filter "Name=tag:Name,Values=${component}" | jq .Reservations[].Instances[].State.Name | sed -s 's/"//g' | grep -E 'run|stop' &>>/dev/null
+ if [ $? -eq 0 ];then
+   echo -e "\e[1;31mThis instamce is already there\e[0m"
+  else
+    aws ec2 run-instances \
+        --launch-template LaunchTemplateId=lt-0078ea8f7c4e4b68e \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${component}}]"
+           if [ $? -ne 0 ];then
+             echo -e "\e[1;32m${component} creation is -failure\e[0m"
+              else
+             echo -e "\e[1;32m${component} creation is -success\e[0m"
+             exit
+           fi
+ fi
+}
+
+
+
+
