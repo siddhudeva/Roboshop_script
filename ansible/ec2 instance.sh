c9ef51765f4300d45cbbe481883851e7a1ef8e3f
siddhu <siddhudangeti@gmail.com>
2021-12-20 12:15:52 +0530

    frontend full setup - ansible

--- ansible/ec2 instance.sh
+++ ansible/ec2 instance.sh
@@ -12,7 +12,7 @@ L_TEMPID=lt-0078ea8f7c4e4b68e
 
 #ec2 instance creating
  aws ec2 describe-instances --filters Name=tag:Name,Values=${component} | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'run|stop' | jq
- if [ $? -eq 0 ]; then
+ if [ $? -ne 0 ]; then
     echo -e "\e[1;31m This instance is already there\e[0m"
   else
 aws ec2 run-instances --launch-template LaunchTemplateId=${L_TEMPID} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${component}}]" | jq
