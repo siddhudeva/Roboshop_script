c554e65285f712f29c5dbd89d72fd578d2d3172f
siddhu <siddhudangeti@gmail.com>
2021-12-29 15:57:46 +0530

    seed.groovy file

--- jenkins/seed.groovy
+++ jenkins/seed.groovy
@@ -15,23 +15,23 @@ freeStyleJob('example') {
     }
 }
 
-pipelineJob('roboshop-ansible') {
-    configure { flowdefinition ->
-        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-                'userRemoteConfigs' {
-                    'hudson.plugins.git.UserRemoteConfig' {
-                        'url'('https://github.com/raghudevopsb61/ansible.git')
-                    }
-                }
-                'branches' {
-                    'hudson.plugins.git.BranchSpec' {
-                        'name'('*/main')
-                    }
-                }
-            }
-            'scriptPath'('Jenkinsfile')
-            'lightweight'(true)
-        }
-    }
-}
+//pipelineJob('roboshop-ansible') {
+//    configure { flowdefinition ->
+//        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//                'userRemoteConfigs' {
+//                    'hudson.plugins.git.UserRemoteConfig' {
+//                        'url'('https://github.com/raghudevopsb61/ansible.git')
+//                    }
+//                }
+//                'branches' {
+//                    'hudson.plugins.git.BranchSpec' {
+//                        'name'('*/main')
+//                    }
+//                }
+//            }
+//            'scriptPath'('Jenkinsfile')
+//            'lightweight'(true)
+//        }
+//    }
+//}
