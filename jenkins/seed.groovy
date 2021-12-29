e6b20c0e08aa9a994f98e596a05ad7662960a960
siddhu <siddhudangeti@gmail.com>
2021-12-29 16:17:24 +0530

    seed.groovy file

--- jenkins/seed.groovy
+++ jenkins/seed.groovy
@@ -1,4 +1,4 @@
-freeStyleJob('example') {
+freeStyleJob('Roboshop') {
     logRotator(-1, 10)
     jdk('Java 8')
     scm {
@@ -14,24 +14,27 @@ freeStyleJob('example') {
         archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
     }
 }
-
-//pipelineJob('roboshop-ansible') {
-//    configure { flowdefinition ->
-//        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//                'userRemoteConfigs' {
-//                    'hudson.plugins.git.UserRemoteConfig' {
-//                        'url'('https://github.com/raghudevopsb61/ansible.git')
-//                    }
-//                }
-//                'branches' {
-//                    'hudson.plugins.git.BranchSpec' {
-//                        'name'('*/main')
-//                    }
-//                }
-//            }
-//            'scriptPath'('Jenkinsfile')
-//            'lightweight'(true)
-//        }
-//    }
-//}
+folder('CI-pipeline') {
+    displayName('CI-pipeline')
+    description('CI-pipeline')
+}
+pipelineJob('CI-pipeline/cart') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/raghudevopsb61/ansible.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
